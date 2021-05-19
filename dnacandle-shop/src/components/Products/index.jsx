dfe5b8abbf56caa338c2ce57b1a69f79e9540d80
iamjayy <jason_doan94@yahoo.com>
2021-05-18 22:19:38 -0400

    style for cards

--- dnacandle-shop/src/components/Products/index.jsx
+++ dnacandle-shop/src/components/Products/index.jsx
@@ -1,11 +1,20 @@
 import Product from "../Product";
+import { Grid, Container } from "@material-ui/core";
 
 const Products = ({ products }) => {
   return (
     <div>
-      {products.map((product) => {
-        return <Product product={product} />;
-      })}
+      <Container id="products">
+        <Grid container spacing={4}>
+          {products.map((product) => {
+            return (
+              <Grid key={product.id} item xs={12} md={4}>
+                <Product product={product} />;
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </div>
   );
 };
