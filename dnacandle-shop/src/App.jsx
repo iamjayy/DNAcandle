c82e1d90ce4a622745ce07504093c3486eebfcc4
iamjayy <jason_doan94@yahoo.com>
2021-05-18 20:51:30 -0400

    product set up

--- dnacandle-shop/src/App.jsx
+++ dnacandle-shop/src/App.jsx
@@ -1,7 +1,31 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Products from "./components/Products";
 
 const App = () => {
-  return <div className="App">app</div>;
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await commerce.products.list();
+    setProducts((response && response.data) || []);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
